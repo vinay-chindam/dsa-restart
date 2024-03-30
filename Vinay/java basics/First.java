4d9f695e48e32191c88050774d07ee40db217b76
vinay-chindam <vinnuhary@gmail.com>
2024-03-30 23:20:24 +0530

    added second program

--- Vinay/java basics/First.java
+++ Vinay/java basics/First.java
@@ -3,6 +3,7 @@ public class First{
     for (int i = 0; i < 10; i++) {
         System.out.println(fib(4));
     }
+    System.out.println(reverse(1534236469));
         
     }
     public static int fib(int n) {
@@ -27,5 +28,27 @@ public class First{
         }
         return ans;
         
+    }
+
+
+    public static int reverse(int x) {
+        int ans=0;
+        int temp=x;
+        if(temp<0){
+            temp=temp*-1;
+        }
+
+        while(temp>0){
+            if ((x >= 0 && ans > Integer.MAX_VALUE / 10) || (x < 0 && -ans < Integer.MIN_VALUE / 10)) {
+                return 0;
+            int rem=temp%10;
+            ans=ans*10;
+            ans=ans+rem;
+            temp=temp/10;
+        }
+        if(x<0){
+            return -1*ans;
+        }
+        return ans;
     }
 }   
